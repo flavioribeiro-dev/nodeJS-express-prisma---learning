7ba5b61996803f1b17653823cdaad7858ac3f79d
fribeiro <flavioxconsult@gmail.com>
2026-09-21 01:39:46 -0300

    criado o arquivo de conexão do Prisma com o BD

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { prisma } from "./libs/prisma.js";
 
 const router = Router();
 
 router.get('/', (req, res) => {
     res.send('ok 12345')
+    // const user = prisma
 })
 
 export default router;
